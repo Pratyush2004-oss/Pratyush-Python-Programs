1c2e5481f9fb55c7f05939d2ff2d99cee1ba0a0c
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-07-24 17:43:35 +0530

    Pyhton

--- CreatingCSV.py
+++ CreatingCSV.py
@@ -1,16 +1,14 @@
 import csv
-fh = open("Student.csv","w")
+fh = open("Student.csv","w",newline='')
 
 stu = csv.writer(fh)
 stu.writerow(["Name" , "Roll Number", "Marks"])
+stulist = []
 for i in range(5):
     name = input("Enter the Name : " )
     roll = int(input("Enter the Roll number : " ))
     marks = int(input("Enter the marks : " ))
-    sturec = [name,roll,marks]
-    stu.writerow(sturec)
+    stulist.append([name,roll,marks])
 
-
-
-
+stu.writerows(stulist)
 fh.close()
